c151d68e0007f2fbc78444dbff3121cb1325c984
AntonPashkowskiy <Wolendrang@gmail.com>
2016-10-21 01:46:03 +0300

    Minor changes for TCP and UDP steps.

--- sft/drivers/tcp/server/steps.py
+++ sft/drivers/tcp/server/steps.py
@@ -1,18 +1,65 @@
 import logging
+from sft.config import Config
+from sft.server.sessions.session_manager import SessionManager
+from sft.server.steps.default import DataNormalizer
 
 
 LOG = logging.getLogger(__name__)
+_config = Config()
+_session_manager = SessionManager()
+
+
+# ------------------ DataReadingStep ------------------
+
+def raw_data_tcp_reader(socket_list):
+    """
+    Receive data from tcp sockets.
+    :param socket_list: List of sockets objects
+    :return: [(client_addr, data), (client_addr, data), ...]
+    """
+
+    buffer_size = _config.tcp_buffer_size
+
+    raw_data = []
+
+    for socket in socket_list:
+        data = socket.recv(buffer_size)
+        client_addr = socket.getpeername()
+
+        if not data:
+            _session_manager.deactivate_session_by_address(client_addr)
+            # ToDo: Close socket in SocketContainer
+            continue
+
+        raw_data.append((client_addr, data))
+
+    return raw_data
+
+# -----------------------------------------------------
+
+
+# ------------------ RawDataNormalizerStep ------------------
+
+# ToDo: How to set this step in the dict, if we need in initialized accumulator for create normalizer?
+class TCPNormalizer(DataNormalizer):
+    """ Send all data into accumulator.
+    """
+
+    def __init__(self, accumulator):
+        super().__init__()
+        self._accumulator = accumulator
+
+    def normalize(self, data):
+        self._accumulator.accumulate_data(data)
+
+# -----------------------------------------------------------
 
 
 steps = {
-    'raw_data_reader': (
-        lambda x: LOG.debug('tcp_raw_data_reader_step'), ),
-    'raw_data_normalizer': (
-        lambda x: LOG.debug('tcp_data_accumulating_step'), ),
+    'raw_data_reader': (raw_data_tcp_reader, ),
+    'raw_data_normalizer': (lambda x: LOG.debug('tcp_data_accumulating_step'), ),
 
-    'packet_data_writer': (
-        lambda x: LOG.debug('tcp_data_writer_step'), ),
+    'packet_data_writer': (lambda x: LOG.debug('tcp_data_writer_step'), ),
 
-    'server_state_check': (
-        lambda x: LOG.debug('tcp_server_state_check_step'), ),
+    'server_state_check': (lambda x: LOG.debug('tcp_server_state_check_step'), ),
 }
--- sft/drivers/udp/server/steps.py
+++ sft/drivers/udp/server/steps.py
@@ -1,18 +1,48 @@
 import logging
+from sft.config import Config
+from sft.server.steps.default import DataNormalizer
 
 
 LOG = logging.getLogger(__name__)
+_config = Config()
+
+
+# ------------------ DataReadingStep ------------------
+
+def raw_data_udp_reader(socket_list):
+    """
+    Receive data from udp sockets.
+    :param socket_list: List of sockets objects
+    :return: [(client_addr, data), (client_addr, data), ...]
+    """
+
+    buffer_size = _config.udp_buffer_size
+    return [socket.recvfrom(buffer_size) for socket in socket_list]
+
+# -----------------------------------------------------
+
+
+# ------------------ RawDataNormalizerStep ------------------
+
+class UDPNormalizer(DataNormalizer):
+
+    def __init__(self, quality_of_service):
+        super().__init__()
+        self._quality_of_service = quality_of_service
+
+    def normalize(self, data):
+        # ToDo: Should contains functionality for QoS.
+
+        raise NotImplementedError()
+
+# -----------------------------------------------------------
 
 
 steps = {
-    'raw_data_reader': (
-        lambda x: LOG.debug('udp_raw_data_reader_step'), ),
-    'raw_data_normalizer': (
-        lambda x: LOG.debug('udp_qos_step'), ),
+    'raw_data_reader': (raw_data_udp_reader, ),
+    'raw_data_normalizer': (lambda x: LOG.debug('udp_qos_step'), ),
 
-    'packet_data_writer': (
-        lambda x: LOG.debug('udp_data_writer_step'), ),
+    'packet_data_writer': (lambda x: LOG.debug('udp_data_writer_step'), ),
 
-    'server_state_check': (
-        lambda x: LOG.debug('udp_server_state_check_step'), ),
+    'server_state_check': (lambda x: LOG.debug('udp_server_state_check_step'), ),
 }
