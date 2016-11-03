c6a3efc94160b2e152a5b2e8d5bc86362de4c7be
AliakseiBuziuma <buzuma.leha@gmail.com>
2016-11-03 15:45:10 +0300

    Moved socket selector step to globel section

--- sft/drivers/tcp/server/steps/_steps.py
+++ sft/drivers/tcp/server/steps/_steps.py
@@ -1,6 +1,5 @@
 import logging
 
-from sft.drivers.tcp.server.steps.socket_selector import socket_selector
 from sft.drivers.tcp.server.steps.raw_data_reader import raw_data_reader
 from sft.drivers.tcp.server.steps.raw_data_normalizer import raw_data_normalizer
 from sft.drivers.tcp.server.steps.server_state_check import server_state_check
@@ -11,7 +10,6 @@ LOG = logging.getLogger(__name__)
 
 
 steps = {
-    'socket_selector': (socket_selector, ),
     'raw_data_reader': (raw_data_reader, ),
     'raw_data_normalizer': (raw_data_normalizer, ),
     'packet_data_writer': (packet_data_writer, ),
