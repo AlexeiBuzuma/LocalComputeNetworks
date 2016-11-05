4f9e9df415bbd30b6961bca7ed8f06f3236ad297
AliakseiBuziuma <buzuma.leha@gmail.com>
2016-11-05 03:30:01 +0300

    Added raw_data_reader and raw_data_normalizer steps for tcp client.

--- sft/drivers/tcp/client/steps/_steps.py
+++ sft/drivers/tcp/client/steps/_steps.py
@@ -1,5 +1,8 @@
 from .raw_data_reader import raw_data_reader
+from .raw_data_normalizer import raw_data_normalizer
+
 
 steps = {
     "raw_data_reader": (raw_data_reader, ),
+    "raw_data_normalizer": (raw_data_normalizer, ),
 }
