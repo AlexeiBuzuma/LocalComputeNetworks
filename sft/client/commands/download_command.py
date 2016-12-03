166ce6b47a3bb0dd86e48207945f4489d90c84d9
AliakseiBuziuma <buzuma.leha@gmail.com>
2016-12-03 05:43:08 +0300

    Modified download command

--- sft/client/commands/download_command.py
+++ sft/client/commands/download_command.py
@@ -1,3 +1,6 @@
+""" This module contains spaghetti implementation of Download command for client.
+"""
+
 import logging
 import argparse
 import shlex
@@ -6,6 +9,8 @@ import os
 
 from .base import ClientCommandBase
 from sft.common.config import Config
+from sft.common.utils.storage import save_client_data, delete_client_data, load_client_data
+from sft.common.sessions.session_manager import SessionManager
 from sft.common.commands.base import CommandFinished, CommandInvalid, CommandIds, ErrorIds
 from sft.common.utils.packets import (generate_packet, generate_header, get_payload,
 get_payload_size, get_header_size, get_error_code)
@@ -36,6 +41,8 @@ class FileWriter:
 
     def __init__(self, path):
         self._path = path
+        self.start_file_size = os.path.getsize(self._path) if os.path.exists(self._path) else 0
+
         self._file_descriptor = open(self._path, "wb")
 
         self._current_index = 0
@@ -54,6 +61,9 @@ class FileWriter:
             self._buffer = bytearray()
             self._current_index = 0
 
+    def seek(self, offset):
+        self._file_descriptor.seek(offset)
+
     def close(self):
         self.stash()
         self._file_descriptor.close()
@@ -71,17 +81,26 @@ class Download(ClientCommandBase):
 
     def _initialize(self, first_packet_data):
         self._start_time = time.time()
-        try:
-            args = _parse_args(first_packet_data)
-        except SystemExit as e:
-            raise CommandInvalid(str(e))
-
         self._create_default_download_dir()
 
-        self._server_file_path = args.file
-        self._client_file_path = os.path.join(STORAGE_PATH, args.file) if args.dest_path is None else args.dest_path
+        self._current_sesion = SessionManager().get_all_not_inactive_sessions()[0]
 
-        self._file_writer = FileWriter(self._client_file_path)
+        if first_packet_data is not None:
+            try:
+                args = _parse_args(first_packet_data)
+            except SystemExit as e:
+                raise CommandInvalid(str(e))
+
+            self._server_file_path = args.file
+            self._client_file_path = os.path.join(STORAGE_PATH, args.file) if args.dest_path is None else args.dest_path
+            self._file_writer = FileWriter(self._client_file_path)
+        else:
+            self._server_file_path = None
+            self._client_file_path = None
+            self._file_writer = None
+
+        self.IS_COMMAND_RESTORED = None
+        self._command_restored_request_sended = None
 
         self._server_file_size = None
         self._readed_bytes = 0
@@ -91,12 +110,42 @@ class Download(ClientCommandBase):
         self._send_approve = False
         self._raise_finished = False
 
+    def _write_client_data(self):
+        client_data = dict(
+            client_uuid=self._current_sesion.client_uuid,
+            server_file_path=self._server_file_path,
+            client_file_path=self._client_file_path,
+            file_size=self._file_size,
+        )
+
+        save_client_data(client_data)
+
+    def _load_client_data(self):
+        client_data = load_client_data()
+
+        if client_data is None:
+            raise Exception("Can't read client data")
+
+        self._server_file_path = client_data["server_file_path"]
+        self._client_file_path = client_data["client_file_path"]
+        self._file_size = client_data["file_size"]
+
+        self._file_writer = FileWriter(self._client_file_path)
+
     @staticmethod
     def _create_default_download_dir():
         if not os.path.exists(STORAGE_PATH):
             os.mkdir(STORAGE_PATH)
 
     def receive_data(self, data):
+
+        if self.IS_COMMAND_RESTORED:
+            error_code = get_error_code(data)
+            if error_code != ErrorIds.DOWNLOAD_POINTER_MOVED:
+                return
+            else:
+                self.IS_COMMAND_RESTORED = False
+                return
 
         if self._first_recived_package:
             if get_error_code(data) == ErrorIds.ERROR:
@@ -105,6 +154,8 @@ class Download(ClientCommandBase):
 
             self._first_recived_package = False
             self._file_size = get_payload_size(data)
+
+            self._write_client_data()
 
             if self._file_size + get_header_size() <= _config.package_size:
                 self._file_writer.write(data[get_header_size():self._file_size+get_header_size()])
@@ -122,8 +173,23 @@ class Download(ClientCommandBase):
                 self._readed_bytes += len(data)
 
     def generate_data(self):
+        if self.IS_COMMAND_RESTORED and not self._command_restored_request_sended:
+            self._command_restored_request_sended = True
+            self._load_client_data()
+
+            if self._file_writer.start_file_size <= _config.package_size:
+                start_from = 0
+            else:
+                start_from = self._file_writer.start_file_size - _config.package_size
+
+            self._file_writer.seek(start_from)
+
+            return generate_packet(CommandIds.DOWNLOAD_COMMAND_ID, ErrorIds.DOWNLOAD_START_FROM, str(start_from))
+
         if self._raise_finished:
             self._file_writer.close()
+            delete_client_data()
+
             print("Download finished")
             print('time spent: %4fs' % (time.time() - self._start_time))
             raise CommandFinished
